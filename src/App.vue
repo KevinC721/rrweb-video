400f7088374b7c83fc7b6ecc1be7cb7697d0fd6b
caiwenkai <953395721@qq.com>
2022-05-12 18:33:22 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Author: caiwenkai
+ * @Date: 2022-05-12 16:11:51
+ * @LastEditors: caiwenkai
+ * @LastEditTime: 2022-05-12 17:01:48
+ * @Discription: 
+ * @FilePath: \rrweb-video\src\App.vue
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -10,8 +18,49 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      events: [],
+      eventsMatrix: [[]]
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.handleRecord()
+  },
+  methods: {
+    handleRecord() {
+      const that = this
+      const rrweb = require('rrweb')
+      console.log('rrweb', rrweb);
+      rrweb.record({
+        emit(event, isCheckout) {
+        console.log('isCheckoutisCheckout', isCheckout);
+        if (isCheckout) {
+          that.eventsMatrix.push([]);
+          console.log('快照', that.eventsMatrix);
+        }
+          console.log('this.record', event);
+          that.events.push(event)
+        },
+        checkoutEveryNth: 20,
+      });
+    },
+
+    save() {
+      const eventsRes = this.events
+      const body = JSON.stringify({ eventsRes });
+      this.events = [];
+      fetch('http://YOUR_BACKEND_API', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body,
+      });
+    }
   }
 }
 </script>
@@ -21,8 +70,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
